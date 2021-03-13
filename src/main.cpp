a151f25cd33ba18fbebd641795dc188215c54e19
YogPanjarale <yog.panjarale@gmail.com>
2021-03-13 17:03:27 +0530

    setup done

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,6 @@ const char gprsUser[] = "";
 const char gprsPass[] = "";
 // MQTT details
 const char* broker = "broker.hivemq.com";
-
-
 char topicbase[] ="dev";
 
 #include <Pin.h>
@@ -22,11 +20,56 @@ void setup()
   SerialMon.begin(115200);
   delay(10);
   SerialMon.println("Starting...");
-  SerialMon.println("Initializing pins...");
   //setting pins
   p1.init();p2.init();p3.init();p4.init();
+  SerialMon.println("Pins Initialized...");
+  // Set GSM module baud rate
+  TinyGsmAutoBaud(SerialAT, GSM_AUTOBAUD_MIN, GSM_AUTOBAUD_MAX);
+  SerialMon.println("GSM module baud rate Set...");
+  // SerialAT.begin(9600);
+  delay(6000);
 
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  SerialMon.println("Initializing modem...");
+  modem.restart();
+  // modem.init();
 
+  String modemInfo = modem.getModemInfo();
+  SerialMon.print("Modem Info: ");
+  SerialMon.println(modemInfo);
+
+  // Unlock your SIM card with a PIN if needed
+  if ( GSM_PIN && modem.getSimStatus() != 3 ) {
+    modem.simUnlock(GSM_PIN);
+  }
+  SerialMon.print("Waiting for network...");
+  if (!modem.waitForNetwork()) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" success");
+
+  if (modem.isNetworkConnected()) {
+    SerialMon.println("Network connected");
+  }
+  // GPRS connection parameters are usually set after network registration
+    SerialMon.print(F("Connecting to "));
+    SerialMon.print(apn);
+    if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+      SerialMon.println(" fail");
+      delay(10000);
+      return;
+    }
+    SerialMon.println(" success");
+
+  if (modem.isGprsConnected()) {
+    SerialMon.println("GPRS connected");
+  }
+  // MQTT Broker setup
+  mqtt.setServer(broker, 1883);
+  mqtt.setCallback(mqttCallback);
 }
 
 void loop()
